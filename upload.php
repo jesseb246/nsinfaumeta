82ed079b1c5ae655a676bb31ef534bee95536930
Jesse Bowers <jbowers2018@fau.edu>
2021-11-07 16:46:35 +0000

    Updated Input Files

---
+++ upload.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+
+<?php
+
+                ini_set('display_errors', 1);
+                ini_set('display_startup_errors', 1);
+                error_reporting(E_ALL);
+
+
+	/*Get the name of the uploaded file*/
+	$filename = $_FILES['file']['name'];
+
+	/*Choose were to save the file*/
+	$location = "files/".$filename;
+
+	/*Save the file to the local filesystem*/
+	if(move_uploaded_file($_FILES['file']['tmp_name'], $location))
+	{
+    		echo 'File uploaded successfully';
+	}else{
+		echo '<b>Error uploading file.</b>';
+		/*Print any errors*/
+	}	
+?>
+</body>
+</html>
